eceed1bf9b498ae0712397e9caacf24fa72f01de
jeep2198 <jeep2198@gmail.com>
2025-09-14 14:52:39 -0500

    feat(app): create base FastAPI app with Swagger.

---
+++ README.md
@@ -0,0 +1,7 @@
+
+como ejecutar local 
+# crea un venv en tu $HOME
+python3 -m venv ~/venvs/geolocation-api
+
+# actívalo
+source ~/venvs/geolocation-api/bin/activate
---
+++ app/main.py
@@ -0,0 +1,34 @@
+# main.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI(
+    title="Geolocation API",
+    description="API para registrar y consultar ubicaciones de dispositivos GPS",
+    version="1.0.0"
+)
+
+# --- MODELOS ---
+class Location(BaseModel):
+    device_id: str
+    latitude: float
+    longitude: float
+    timestamp: str
+
+# Base temporal en memoria
+locations: List[Location] = []
+
+# --- ENDPOINTS ---
+@app.get("/", tags=["Health"])
+def root():
+    return {"message": "Geolocation API is running 🚀"}
+
+@app.post("/locations", tags=["Locations"])
+def add_location(location: Location):
+    locations.append(location)
+    return {"message": "Location added", "total": len(locations)}
+
+@app.get("/locations", response_model=List[Location], tags=["Locations"])
+def get_locations():
+    return locations
